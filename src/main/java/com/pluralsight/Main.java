fd1828b421923ddd2fceee849af73c04710f4ce7
pmvroque15 <pmvroque@gmail.com>
2026-04-20 15:01:57 -0400

    Added String Array for the files to be looped

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,29 @@
 package com.pluralsight;
 
+import java.io.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+        String[] bedtimeStories = {
+                "godilocks.txt",
+                "mary_had_a_little_lamb.txt",
+                "hansel_and_gretel.txt"
+        };
 
+        try {
+//            FileReader godilocks = new FileReader("src/main/resources/goldilocks.txt");
+//            BufferedReader bufferedReader = new BufferedReader(godilocks);
+
+            String input;
+            while((input = bufferedReader.readLine()) != null) {
+                System.out.println(input);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Sorry, I can't read the file.");
+        } catch (IOException e) {
+            System.err.println("I couldn't find the file");
+        }
     }
 }
